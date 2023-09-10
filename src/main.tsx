6b5a064987b6861d6ffb6f5bb21840a3e2e34d60
SeungGukYoo <dbtmd878@gmail.com>
2023-09-10 16:54:28 +0900

    feat: 최상위레벨에서의 전역상태, 클래스 모듈, chartJS 초기화

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from 'chart.js';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+import { AreaStoreProvider } from './context/areaStoreContext.tsx';
+import './index.css';
+import { ConvertData } from './util/covertData.ts';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+const convertData = new ConvertData();
+root.render(
+  <AreaStoreProvider convertData={convertData}>
     <App />
-  </React.StrictMode>,
+  </AreaStoreProvider>,
 );
