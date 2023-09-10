d661e108bc12f6bfd4faa5943e1030f61de0fa8c
SeungGukYoo <dbtmd878@gmail.com>
2023-09-10 16:51:42 +0900

    feat: 지역 정보를 관리하기 위한 전역 context 추가

---
+++ src/context/areaStoreContext.tsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import type { Props, TResponse } from '../..';
+
+export const AreaStoreContext = createContext<TResponse | null>(null);
+
+export const useAreaData = () => useContext(AreaStoreContext);
+
+export function AreaStoreProvider({ children, convertData }: Props) {
+  const [areaDatas, setAreaDatas] = useState({});
+
+  useEffect(() => {
+    const callData = async () => {
+      const result = await convertData.getAreaData('mock.json');
+      console.info(result);
+    };
+    callData();
+  }, [convertData]);
+
+  return <AreaStoreContext.Provider value={areaDatas}>{children}</AreaStoreContext.Provider>;
+}
