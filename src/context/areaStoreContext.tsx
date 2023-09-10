2bfae30ce986426e45c67ec8099800d45042530f
SeungGukYoo <dbtmd878@gmail.com>
2023-09-11 02:44:17 +0900

    feat: 전역 state에 데이터 저장

--- src/context/areaStoreContext.tsx
+++ src/context/areaStoreContext.tsx
@@ -1,20 +1,27 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import type { Props, TResponse } from '../..';
+import type { IData, Props, TResponse } from '../..';
+
+interface remakeData {
+  [name: string]: IData;
+}
 
 export const AreaStoreContext = createContext<TResponse | null>(null);
 
 export const useAreaData = () => useContext(AreaStoreContext);
 
 export function AreaStoreProvider({ children, convertData }: Props) {
-  const [areaDatas] = useState({});
+  const [areaDate, setAreaDate] = useState<remakeData>({});
 
   useEffect(() => {
     const callData = async () => {
       const result = await convertData.getAreaData('mock.json');
-      console.info(result);
+      setAreaDate(pre => {
+        pre = result;
+        return pre;
+      });
     };
     callData();
   }, [convertData]);
 
-  return <AreaStoreContext.Provider value={areaDatas}>{children}</AreaStoreContext.Provider>;
+  return <AreaStoreContext.Provider value={{ areaDate }}>{children}</AreaStoreContext.Provider>;
 }
