d99121cc99a82539ab3cf92e65f1261625d37690
SeungGukYoo <dbtmd878@gmail.com>
2023-09-10 16:51:09 +0900

    feat: Bar 컴포넌트 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { ChartData } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
 function App() {
+  const data: ChartData<'bar'> = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
     <>
       <h1 className="text-red-400">react start</h1>
+
+      <div className=" max-w-[1024px]">
+        <Bar data={data} />
+      </div>
     </>
   );
 }
