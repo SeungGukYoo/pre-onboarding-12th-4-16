1c8c78e2eaced3e25b62f178f3f1c37af20b34bc
SeungGukYoo <dbtmd878@gmail.com>
2023-09-11 16:24:30 +0900

    feat: 응답받은 데이터를 사용하기 편한 형태로 변환하여 반환하는 기능 추가

--- src/util/convertData.ts
+++ src/util/convertData.ts
@@ -1,9 +1,40 @@
-export class ConvertData {
-  async getAreaData(path: string) {
-    const response = await fetch(`/data/${path}`);
-    const json = await response.json();
-    console.log(json);
+import { TResponse } from '../..';
 
-    return json.response;
+type DataObj = {
+  id: string[];
+  labels: string[];
+  bar: number[];
+  area: number[];
+};
+
+type ParsedJson = {
+  response: TResponse;
+};
+
+export class ConvertData {
+  async getData(path: string) {
+    const response = await fetch(`/data/${path}`);
+    const json: ParsedJson = await response.json();
+
+    const result = this.convertDataStructor(json.response);
+
+    return result;
+  }
+
+  convertDataStructor(json: TResponse) {
+    const DataObj: DataObj = {
+      id: [],
+      labels: [],
+      bar: [],
+      area: [],
+    };
+
+    for (const data in json) {
+      DataObj.id.push(json[data].id);
+      DataObj.labels.push(data.split(' ')[1]);
+      DataObj.bar.push(json[data].value_bar);
+      DataObj.area.push(json[data].value_area);
+    }
+    return DataObj;
   }
 }
