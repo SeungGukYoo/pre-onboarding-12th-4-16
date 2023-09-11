870f9f75bf8b5665977b6d2100c475fd458b24d5
SeungGukYoo <dbtmd878@gmail.com>
2023-09-11 22:14:54 +0900

    feat : 배경색 변경 함수 구현, 지역 Set추가, 배경색 배열 형태로 변경

--- src/hook/useChartData.tsx
+++ src/hook/useChartData.tsx
@@ -1,8 +1,7 @@
 import { useAreaData } from '../context/areaStoreContext';
 
 function useChartData() {
-  const areaData = useAreaData()?.areaData;
-
+  const { areaData, changeColor, currentFocusLocation } = useAreaData();
   const labels = areaData?.labels;
 
   const makeChart = (type: 'bar' | 'line') => {
@@ -10,7 +9,7 @@ function useChartData() {
       label: 'BAR VALUE',
       data: areaData?.bar,
       borderWidth: 1,
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      backgroundColor: areaData?.barColor,
       borderColor: 'rgba(255, 99, 132, 1)',
       yAxisID: 'barValue',
     };
@@ -18,31 +17,15 @@ function useChartData() {
     if (type === 'line') {
       defaultChart.label = 'AREA VALUE';
       defaultChart.data = areaData?.area;
-      defaultChart.borderWidth = 3;
-      defaultChart.backgroundColor = 'rgba(75, 192, 192, 0.5)';
+      defaultChart.borderWidth = 1;
+
+      defaultChart.backgroundColor = ['rgba(75, 192, 192, 0.7)'];
       defaultChart.borderColor = 'rgba(75, 192, 192, 1)';
       defaultChart.yAxisID = 'areaValue';
     }
 
     return defaultChart;
   };
-  // const makeOption = (id: string, type: 'bar' | 'line') => {
-  // const scales: OptionType = {
-  //   id: '',
-  // };
-  // if (type === 'bar') {
-  //   scales[id] = {};
-  //   scales[id].max = 20000;
-  //   scales[id].position = 'left';
-  // } else {
-  //   scales[id] = {};
-  //   scales[id].max = 200;
-  //   scales[id].position = 'right';
-  // }
-  // scales[id].min = 0;
-  // scales[id].beginAtZero = true;
-  // return scales;
-  // };
 
   const printTooltip = (index: number, type: 'bar' | 'line') => {
     const message = `location: ${areaData?.id[index]} Value: ${
@@ -50,7 +33,34 @@ function useChartData() {
     }`;
     return message;
   };
-  return { labels, printTooltip, makeChart };
+
+  const Location = [...new Set(areaData?.id)];
+
+  const getClickIndex = (index: number) => {
+    if (!index) return;
+    filtering(areaData.id[index]);
+  };
+  const filtering = (areaName: string) => {
+    if (currentFocusLocation === areaName) {
+      const newAnswer = areaData?.id.map((data, index) => {
+        areaData.barColor[index] = 'rgba(255, 99, 132, 0.2)';
+        return data;
+      });
+      if (newAnswer && changeColor) changeColor(areaName, newAnswer);
+    } else {
+      const newAnswer = areaData?.id.map((data, index) => {
+        if (data === areaName) {
+          areaData.barColor[index] = 'rgba(255, 99, 132, 1)';
+        } else {
+          areaData.barColor[index] = 'rgba(255, 99, 132, 0.2)';
+        }
+        return data;
+      });
+      if (newAnswer && changeColor) changeColor(areaName, newAnswer);
+    }
+  };
+
+  return { labels, printTooltip, makeChart, Location, filtering, getClickIndex };
 }
 
 export default useChartData;
